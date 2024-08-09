9a7fd4739698f95458c55372f1e4c209590dd7d3
nicolas valdes <nicolas.valdes@live.com>
2024-08-09 11:57:18 -0400

    creacion de formulario

--- temarioApp/views.py
+++ temarioApp/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Bienvenido a la página principal de la app desafio Kiss.")
 
+def index(request):
+    contexto = {'mensaje': 'Hola desde Django!'}
+    return render(request, 'temarioApp/base.html', contexto)
+
+
+def crear_curso(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'temarioApp/crear_curso.html')
